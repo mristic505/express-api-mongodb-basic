0a9fd03780ba405e556c097619ee4bc3fda3ea7c
Mateja Ristic <mristic@brandconnections.com>
2018-10-10 20:17:16 -0400

    move main code to index.js

---
+++ models/customer.js
@@ -0,0 +1,29 @@
+const Joi = require('joi');
+const mongoose = require('mongoose');
+
+// Create Schema for a collection
+const customerSchema = new mongoose.Schema({
+    name: {
+      type: String,
+      required: true
+    },
+    isGold: Boolean,
+    phone: Number
+});
+
+// Create Class based on the schema
+const Customer = mongoose.model('Customer', customerSchema, 'customers');
+
+// VALIDATE WITH Joi FUNCTION
+function validateCustomer(customer) {
+    const schema = {
+      name: Joi.string().min(5).max(50).required(),
+      isGold: Joi.boolean(),
+      phone: Joi.string().min(5).max(50).required(),
+    };
+  
+    return Joi.validate(customer, schema);
+  }
+
+Exports.Customer = Customer;
+Exports.validateCustomer = validateCustomer;
--- routes/customers.js
+++ routes/customers.js
@@ -1,33 +1,19 @@
-const Joi = require('joi');
 const mongoose = require('mongoose');
 const express = require('express');
 const router = express.Router();
 
-// Create Schema for a collection
-const customerSchema = new mongoose.Schema({
-    name: {
-      type: String,
-      required: true
-    },
-    isGold: Boolean,
-    phone: Number
-});
-
-// Create Class based on the schema
-const CustomerMDBModel = mongoose.model('CustomerMDBModel', customerSchema, 'customers');
-
 // GET ======================================
 router.get('/', async (req, res) => {
-  let customers = await CustomerMDBModel.find()
+  let customers = await Customer.find()
   res.send(customers);
 });
 
 // POST =====================================
 router.post('/', async (req, res) => {
-  const { error } = validateCustomerMDBModel(req.body); 
+  const { error } = validateCustomer(req.body); 
   if (error) return res.status(400).send(error.details[0].message);
 
-  let customer = new CustomerMDBModel({    
+  let customer = new Customer({    
     name: req.body.name,
     isGold: req.body.isGold,
     phone: req.body.phone
@@ -41,14 +27,14 @@ router.post('/', async (req, res) => {
 router.put('/:id', async (req, res) => {
 
   // Validate provided value with JOI
-  const { error } = validateCustomerMDBModel(req.body); 
+  const { error } = validateCustomer(req.body); 
   if (error) return res.status(400).send(error.details[0].message);
 
   // Validate ID format and length
   idFormatNotValid(req.params.id) && res.status(404).send('Invalid ID format');
 
   // Look for document in DB and update if found
-  let customer = await CustomerMDBModel.findByIdAndUpdate(req.params.id, {name: req.body.name}, {new: true});
+  let customer = await Customer.findByIdAndUpdate(req.params.id, {name: req.body.name}, {new: true});
   // If document not found in DB
   if (!customer) return res.status(404).send('The customer with the given ID was not found.');
 
@@ -58,14 +44,14 @@ router.put('/:id', async (req, res) => {
 // DELETE ===================================
 router.delete('/:id', async (req, res) => {
   // Validate provided value with JOI
-  const { error } = validateCustomerMDBModel(req.body); 
+  const { error } = validateCustomer(req.body); 
   if (error) return res.status(400).send(error.details[0].message);
 
   // Validate ID format and length
   idFormatNotValid(req.params.id) && res.status(404).send('Invalid ID format');
 
   // Look for document in DB and update if found
-  let customer = await CustomerMDBModel.findByIdAndRemove(req.params.id);
+  let customer = await Customer.findByIdAndRemove(req.params.id);
   // If document not found in DB
   if (!customer) return res.status(404).send('The customer with the given ID was not found.');
 
@@ -77,21 +63,11 @@ router.get('/:id', async (req, res) => {
   // Validate ID format and length
   idFormatNotValid(req.params.id) && res.status(404).send('Invalid ID format');
   // Look for document in DB
-  const customer = await CustomerMDBModel.findById(req.params.id);
+  const customer = await Customer.findById(req.params.id);
   // Send response
   customer? res.send(customer) : res.status(404).send('The customer with the given ID was not found.');
 });
 
-// VALIDATE WITH Joi FUNCTION
-function validateCustomerMDBModel(customer) {
-  const schema = {
-    name: Joi.string().min(3).required(),
-    isGold: Joi.boolean(),
-    phone: Joi.number()
-  };
-
-  return Joi.validate(customer, schema);
-}
 
 const idFormatNotValid = (id) => {
     // Validate ID format and length
